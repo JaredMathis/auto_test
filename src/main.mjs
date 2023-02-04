86c75776b2b9558749744ac642523ce986ed3363
JaredMathis <jared.mathis@native.consulting>
2023-02-04 00:09:35 -0500

    function_auto main

--- src/main.mjs
+++ src/main.mjs
@@ -2,7 +2,7 @@ import { element } from './../node_modules/m00/src/element.mjs';
 import { defaults_set } from './../node_modules/m00/src/defaults/set.mjs';
 import { element_input } from './../node_modules/m00/src/element/input.mjs';
 export function main() {
-    defaults_set(element.name, { width: '100%' });
+    defaults_set(element.name, { fontSize: '5vh' });
     defaults_set(element_input.name, { width: '100%' });
-    element_input(document.body);
+    let input = element_input(document.body);
 }
